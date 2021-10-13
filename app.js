49cfaa3b849d766ef320c816e1fe1e4c645d3265
JonathanHill513 <jonnyhill513@gmail.com>
2021-10-12 20:47:00 -0400

    push

---
+++ app.js
@@ -0,0 +1,41 @@
+var num1= 20;
+var num2= 4;
+
+
+function getSum (a,b){
+    var sum = num1 + num2
+    console.log(sum);
+    return sum;
+}
+var mySum = getSum(3,2) ;
+console.log("The Sum here is... " + mySum);
+
+
+
+function getDif (a,b){
+    var Dif = num1 - num2
+    console.log(Dif);
+    return Dif;
+}
+var myDif = getDif(1,2) ;
+console.log("The Difference here is... " + myDif);
+
+
+
+function getMultipy (a,b){
+    var Multipy = num1 * num2
+    console.log(Multipy);
+    return Multipy;
+}
+var myMultipy = getMultipy(1,2) ;
+console.log("The Multiplication here is... " + myMultipy);
+
+
+
+function getDivide (a,b){
+    var Divide = num1 / num2
+    console.log(Divide);
+    return Divide;
+}
+var myDivide = getDivide(1,2) ;
+console.log("The Division here is... " + myDivide);
